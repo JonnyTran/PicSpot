8f9abeee3eede202126f0410da75feea76d5559c
Nhat Tran <nhat.tran@mavs.uta.edu>
2014-11-21 18:03:38 -0600

    Rotation matrix is implemented with gyrosensor

--- src/com/alphaforce/picspot/MainActivity.java
+++ src/com/alphaforce/picspot/MainActivity.java
@@ -1,21 +1,40 @@
 package com.alphaforce.picspot;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 
 public class MainActivity extends Activity {
 
+	private SensorManager mSensorManager;
 	private GLSurfaceView mGLView;
+	private MyGLRenderer mRenderer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
+		// Get sensor service
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
 		// Create a GLSurfaceView instance and set it
 		// as the ContentView for this Activity
-		mGLView = new MyGLSurfaceView(this);
+		mGLView = new GLSurfaceView(this);
+		
+		// Create an OpenGL ES 3.0 context.
+		mGLView.setEGLContextClientVersion(3);
+
+		// Set the Renderer for drawing on the GLSurfaceView
+		mRenderer = new MyGLRenderer();
+		mGLView.setRenderer(mRenderer);
+		
+		// Render the view only when there is a change in the drawing data
+		mGLView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+		
 		setContentView(mGLView);
 	}
 
@@ -27,6 +46,9 @@ public class MainActivity extends Activity {
 		// you should consider de-allocating objects that
 		// consume significant memory here.
 		mGLView.onPause();
+
+		// Unregister sensor
+		mSensorManager.unregisterListener(mRenderer);
 	}
 
 	@Override
@@ -36,5 +58,56 @@ public class MainActivity extends Activity {
 		// If you de-allocated graphic objects for onPause()
 		// this is a good place to re-allocate them.
 		mGLView.onResume();
+
+		// Register sensors
+		mSensorManager.registerListener(
+				mRenderer, 
+				mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+				SensorManager.SENSOR_DELAY_GAME );
+		mSensorManager.registerListener(
+				mRenderer, 
+				mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), 
+				SensorManager.SENSOR_DELAY_GAME );
 	}
+
+	private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+	private float mPreviousX;
+	private float mPreviousY;
+
+	@Override
+	public boolean onTouchEvent(MotionEvent e) {
+		// MotionEvent reports input details from the touch screen
+		// and other input controls. In this case, you are only
+		// interested in events where the touch position changed.
+
+		float x = e.getX();
+		float y = e.getY();
+
+		switch (e.getAction()) {
+		case MotionEvent.ACTION_MOVE:
+
+			float dx = x - mPreviousX;
+			float dy = y - mPreviousY;
+
+			// reverse direction of rotation above the mid-line
+			if (y > mGLView.getHeight() / 2) {
+				dx = dx * -1 ;
+			}
+
+			// reverse direction of rotation to left of the mid-line
+			if (x < mGLView.getWidth() / 2) {
+				dy = dy * -1 ;
+			}
+
+			mRenderer.setAngle(
+					mRenderer.getAngle() +
+					((dx + dy) * TOUCH_SCALE_FACTOR));  // = 180.0f / 320
+			mGLView.requestRender();
+		}
+
+		mPreviousX = x;
+		mPreviousY = y;
+		return true;
+	}
+
 }
--- src/com/alphaforce/picspot/MyGLRenderer.java
+++ src/com/alphaforce/picspot/MyGLRenderer.java
@@ -3,26 +3,40 @@ package com.alphaforce.picspot;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.opengl.GLES30;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLU;
+import android.opengl.Matrix;
 
-public class MyGLRenderer implements GLSurfaceView.Renderer {
+public class MyGLRenderer implements GLSurfaceView.Renderer, SensorEventListener {
 
-    private final float[] mProjectionMatrix = new float[16];
+	private float[] gravity = new float[3];
+	private float[] geomag = new float[3];
+	
+	// mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    private float[] mMVPMatrix = new float[16];
+    private float[] mProjectionMatrix = new float[16];
+    private float[] mViewMatrix = new float[16];
+    private float[] mRotationMatrix = new float[16];
+    
+	private float mAngle;
+    
 	private Triangle mTriangle;
 	
 	/* 
 	 * Called once to set up the view's OpenGL ES environment
 	 */
 	@Override
-	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+	public void onSurfaceCreated(GL10 unused, EGLConfig config) {
 
-		// Set background color
-		GLES30.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+		// Set the background frame color
+		GLES30.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		
 	    mTriangle = new Triangle();
-
 	}
 
 	/* 
@@ -33,11 +47,17 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
 		GLES30.glViewport(0, 0, width, height);
 
-		// make adjustments for screen ratio
+		// Make adjustments for screen ratio
 		float ratio = (float) width / height;
-		gl.glMatrixMode(GL10.GL_PROJECTION);        // set matrix to projection mode
-		gl.glLoadIdentity();                        // reset the matrix to its default state
-		gl.glFrustumf(-ratio, ratio, -1, 1, 3, 7);  // apply the projection matrix
+		
+		// Edit frustum cone
+        Matrix.frustumM(mProjectionMatrix, 0, 
+        		-ratio, 	// left
+        		ratio, 		// right
+        		-1, 		// bottom
+        		1, 			// top
+        		3f, 		// near
+        		10f);		// far
 	}
 
 	/* 
@@ -45,10 +65,112 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 	 */
 	@Override
 	public void onDrawFrame(GL10 gl) {
-		// Redraw background color
-		GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT);
+		float[] scratch = new float[16];
+		
+		//apply rotation matrix
+		gl.glLoadMatrixf(mRotationMatrix, 0);
 
-		mTriangle.draw();
+		/*set default camera, 2 units of the ground, 
+		with the up vector pointing positively on the y axis.*/
+		GLU.gluLookAt(gl, 0, 0, 2, 0, 0, 0, 0, 1, 0);
+		
+		// Redraw background color
+		GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT | GLES30.GL_DEPTH_BUFFER_BIT);
+
+		// Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, 
+        		0, 		0, 		0, 	// eye
+        		0f, 	0f, 	1.0f, 	// center of view
+        		0.0f, 	1.0f, 	0.0f);	// up vector
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+		
+        // Create rotation matrix
+//        Matrix.setRotateM(mRotationMatrix, 0, mAngle, 
+//        		1.0f, 	0, 		0);
+
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the mMVPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+
+        mTriangle.draw(scratch);
 	}
 	
+	/**
+     * Utility method for compiling a OpenGL shader.
+     *
+     * <p><strong>Note:</strong> When developing shaders, use the checkGlError()
+     * method to debug shader coding errors.</p>
+     *
+     * @param type - Vertex or fragment shader type.
+     * @param shaderCode - String containing the shader code.
+     * @return - Returns an id for the shader.
+     */
+	public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES30.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES30.glShaderSource(shader, shaderCode);
+        GLES30.glCompileShader(shader);
+        
+        return shader;
+    }
+	
+	/**
+     * Returns the rotation angle of the triangle shape (mTriangle).
+     *
+     * @return - A float representing the rotation angle.
+     */
+    public float getAngle() {
+        return mAngle;
+    }
+
+    /**
+     * Sets the rotation angle of the triangle shape (mTriangle).
+     */
+    public void setAngle(float angle) {
+        mAngle = angle;
+    }
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		int type= event.sensor.getType();
+
+		//Smoothing the sensor data a bit
+		if (type == Sensor.TYPE_MAGNETIC_FIELD) {
+			geomag[0]=(geomag[0]*1+event.values[0])*0.5f;
+			geomag[1]=(geomag[1]*1+event.values[1])*0.5f;
+			geomag[2]=(geomag[2]*1+event.values[2])*0.5f;
+		} else if (type == Sensor.TYPE_ACCELEROMETER) {
+			gravity[0]=(gravity[0]*2+event.values[0])*0.33334f;
+			gravity[1]=(gravity[1]*2+event.values[1])*0.33334f;
+			gravity[2]=(gravity[2]*2+event.values[2])*0.33334f;
+		}
+
+		if ((type==Sensor.TYPE_MAGNETIC_FIELD) || (type==Sensor.TYPE_ACCELEROMETER)) {
+			mRotationMatrix = new float[16];
+			SensorManager.getRotationMatrix(mRotationMatrix, null, gravity, geomag);
+			SensorManager.remapCoordinateSystem( 
+					mRotationMatrix, 
+					SensorManager.AXIS_Y, 
+					SensorManager.AXIS_MINUS_X, 
+					mRotationMatrix );
+		}
+		
+		for (int i = 0; i < mRotationMatrix.length; i++) {
+			System.out.print(mRotationMatrix[i] + ", ");
+		}
+		System.out.println();		
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
 }
